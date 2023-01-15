221befed44ca0319ea780dbc23425c23304c4267
EvgenyYuzhny <114745055+EvgenyYuzhny@users.noreply.github.com>
2023-01-15 12:37:30 +0300

    Почистить ненужные комментарии

--- scripts/index.js
+++ scripts/index.js
@@ -202,19 +202,6 @@ popups.forEach((popup) => {
     })
 });
 
-/*
-
-// Запускаем валидацию из файла validation.js
-enableValidation({
-  formSelector: ".popup__form",
-  inputSelector: ".popup__input", 
-  submitButtonSelector: ".popup__save", 
-  inactiveButtonClass: "popup__save_disabled",
-  inputErrorClass: "popup__input_type_error", 
-  errorClass: "popup__input-error_active", 
-});
-*/
-
 const enableConfig = {
   formSelector: ".popup__form",
   inputSelector: ".popup__input", 
